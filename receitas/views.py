b9ddfbdf2985f4d0b969fa4ce98c3e9146b54117
Matheus Lucca <matheusconts00@gmail.com>
2023-07-17 17:09:31 -0300

    Foram criados os Templates de home

--- receitas/views.py
+++ receitas/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
-# from django.shortcuts import render
+from django.shortcuts import render
 
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('HOME 1')
+    return render(request, 'receitas/home.html')
+
 
 def sobre(request):
     return HttpResponse('SOBRE 1')

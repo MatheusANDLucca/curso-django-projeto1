cff7f16478ad65f5472e368d7701bba91e7ccc2c
Matheus Lucca <matheusconts00@gmail.com>
2023-07-18 10:58:24 -0300

    Separando Head em partials e Home em Pages

--- receitas/views.py
+++ receitas/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'receitas/home.html', context={
+    return render(request, 'receitas/pages/home.html', context={
         'name': 'Matheus Lucca',
     })

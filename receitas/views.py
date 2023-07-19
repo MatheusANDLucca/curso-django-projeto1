522035237c6776d6f958e2df6d0797f227656df2
Matheus Lucca <matheusconts00@gmail.com>
2023-07-19 16:49:32 -0300

    Descrição, titulo, foto adição.

--- receitas/views.py
+++ receitas/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from utils.receitas.factory import make_recipe
 
 def home(request):
     return render(request, 'receitas/pages/home.html', context={
-        'name': 'Matheus Lucca',
+
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 def receita(request, id):
     return render(request, 'receitas/pages/receita-view.html', context={
-        'name': 'Matheus Lucca',
+        'recipe': make_recipe(),
     })

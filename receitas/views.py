66ce85957e00dcd79dfaac9c7d1314ceeb4e0639
Matheus Lucca <matheusconts00@gmail.com>
2023-07-20 15:00:23 -0300

    Mudando titulo pagina de categoria

--- receitas/views.py
+++ receitas/views.py
@@ -3,14 +3,19 @@ from utils.receitas.factory import make_recipe
 from .models import Recipe
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'receitas/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'receitas/pages/home.html', context={
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True
+        ).order_by('-id')
+    return render(request, 'receitas/pages/categoria.html', context={
         'recipes': recipes,
     })
 
